7315a241a931a31f562310fec5956d06af023a51
Alex Taggart <63368154+AlexTagg392@users.noreply.github.com>
2020-08-19 18:33:57 -0500

    Update Projects.js

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -4,15 +4,10 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import "./projectsStyle.css"
-import Image from "../pages/assets/background.jpg";
+import Image from "../pages/assets/background2.jpg";
 import SimplySomm from "../pages/assets/SimplySomm.png";
 import CineSearch from "../pages/assets/cinesearch.png";
-import DayPlanner from "../pages/assets/dayplan.png";
-import Password from "../pages/assets/Password.png";
-import Office from "../pages/assets/Office.jpg";
-import Taker from "../pages/assets/Taker.png";
-
-
+import ToolNextDoor from "../pages/assets/ToolNextDoor.png"
 
 function Projects() {
   return (
@@ -27,35 +22,85 @@ function Projects() {
           <Col size="md-12">
             <h1>Welcome To My Projects!</h1>
             {/* <h4>Click on Projects to see my work!</h4> */}
-            <h6><a className="links" href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">LinkedIn</a>  |  <a href="https://docs.google.com/document/d/1HSC-ZWWhYmK09Sp2Q9dplR12UchosU7bdGmLnQmPB40/edit?usp=sharing" className="links"> Resume</a>  </h6>
-            <h6><a className="links" href="https://github.com/AlexTagg392">GitHub</a></h6>
+            <h4><a className="links" href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">LinkedIn</a>  |  <a href="https://docs.google.com/document/d/1HSC-ZWWhYmK09Sp2Q9dplR12UchosU7bdGmLnQmPB40/edit?usp=sharing" className="links"> Resume</a>  </h4>
+            <h4><a className="links" href="https://github.com/AlexTagg392">GitHub</a></h4>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
             <hr />
-            <h2>Projects</h2>
+            <h1>Projects</h1>
           </Col>
           <Col size="sm-12">
-            <div class="col-sm-6">
+          <div class="card">
+                <img class="card-img" src={ToolNextDoor} alt="Tool Next Door"/>
+                <div class="card-body">
+                  <h4 class="card-title">Tool Next Door</h4>
+                  <div>
+                    <p class="text-title">Description</p>
+                    <p class=" text">
+                      Tool Next Door is a Community Application of do-it-yourselfers that benefit from each other needs. 
+                      
+                    </p>
+                    <p class="text-title">Concepts and Technologies</p>
+                    <p class=" text">
+                      HTML, CSS, JavaScript, MongoDB, REACT, Express, Node.js, Mongoose, Heroku, Material UI and Stripe.
+                      
+                    </p>
+                  </div>
+                  <div class="button">
+                  <a href="https://shielded-temple-94953.herokuapp.com/" class="btn btn-primary">Tool Next Door! 
+                  </a>
+                  <a href="https://github.com/AlexTagg392/ToolNextDoor" class="btn btn-primary">Click Here for the Repo </a>
+                  </div>
+                </div>
+              </div>
+            {/* <div class="col-sm-6"> */}
               <div class="card">
                 <img class="card-img-top" src={SimplySomm} alt="Simply Somm"/>
                 <div class="card-body">
-                  <h4 class="card-title">Simply Somm (Project 2)</h4>
+                  <h4 class="card-title">Simply Somm </h4>
+                  <div>
+                    <p class="text-title">Description</p>
+                    <p class=" text">
+                    Simply Somm is an Application that creates a list of whiskey or wine based on a user's picks in Aroma and Taste.
+                      
+                    </p>
+                    <p class="text-title">Concepts and Technologies</p>
+                    <p class=" text">
+                    HTML, CSS, Node.js, Sequelize, jQuery, JavaScript, Express.js, Express-Handlebars, Path, and Bootstrap. 
+                      
+                    </p>
+                  </div>
+                  <div class="button">
                   <a href="https://metric-drake-50737.herokuapp.com/" class="btn btn-primary">Simply Somm! 
                   </a>
-                  <a href="https://github.com/KingKitty89/project2" class="btn btn-primary">Click Here for the Repo </a>
+                  <a href="https://github.com/AlexTagg392/SimplySomm" class="btn btn-primary">Click Here for the Repo </a>
+                </div>
                 </div>
               </div>
-            </div>
-            <div class="col-sm-6">
+            {/* </div> */}
+            {/* <div class="col-sm-6"> */}
               <div class="card">
                 <img class="card-img-top" src={CineSearch} alt="CineSearch"/>
                 <div class="card-body">
-                  <h4 class="card-title">CineSearch (Project 1)</h4>
+                  <h4 class="card-title">CineSearch </h4>
+                  <div>
+                    <p class="text-title">Description</p>
+                    <p class=" text">
+                    CineSearch is an Application that allows you to find a list of movies that fits your search criteria
+                      
+                    </p>
+                    <p class="text-title">Concepts and Technologies</p>
+                    <p class=" text">
+                    HTML, CSS, JavaScript, jQuery, Foundation Framework and Multiple Movie APIs(OMDB, TMDB and UTelly)
+                      
+                    </p>
+                  </div>
+                  <div class="button">
                   <a href="https://alextagg392.github.io/CineSearch/" class="btn btn-primary">CineSearch! 
                   </a>
-                  <a href="https://github.com/AlexTagg392/CineSearch" class="btn btn-primary">Repo </a>
+                  <a href="https://github.com/AlexTagg392/CineSearch" class="btn btn-primary">Click Here for the Repo </a>
                 </div>
               </div>
             </div>
@@ -64,50 +109,16 @@ function Projects() {
         <Row>
           <Col size="md-12">
             <hr />
-            <h2>Exemplar Examples</h2>
+            <h2>Contact</h2>
           </Col>
           <Col size="md-12">
-              <div class="col-md-4">
-                <div class="card">
-                  <img class="card-img-top" src={DayPlanner} alt="Day Planner"/>
-                  <div class="card-body">
-                    <h4 class="card-title">Day Planner </h4>
-                    <a href="https://alextagg392.github.io/Homework_Day-Planner/" class="btn btn-primary"> Day Planner </a>
-                    <a href="https://github.com/AlexTagg392/Day-Planner" class="btn btn-primary"> Repo </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card">
-                  <img class="card-img-top" src={Password} alt="Password Generator"/>
-                  <div class="card-body">
-                    <h4 class="card-title">Password Generator </h4>
-                    <a href="https://alextagg392.github.io/Homework_Day-Planner/" class="btn btn-primary"> Password Generator </a>
-                    <a href="https://github.com/AlexTagg392/Homework_Password-Generator" class="btn btn-primary"> Repo </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-              <div class="card">
-                <img class="card-img-top" src={Office} alt="Office"/>
-                <div class="card-body">
-                  <h4 class="card-title">"The Office" Management System </h4>
-                  <a href="https://github.com/AlexTagg392/DunderMifflinEmployees" class="btn btn-primary"> Office Management System </a>
-                  <a href="https://github.com/AlexTagg392/DunderMifflinEmployees" class="btn btn-primary"> Repo </a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card">
-                <img class="card-img-top" src={Taker} alt="GoT"/>
-                <div class="card-body">
-                  <h4 class="card-title">The Taker of Notes</h4>
-                  <a href="https://github.com/AlexTagg392/Taker-of-Notes" class="btn btn-primary"> Taker of Notes </a>
-                  <a href="https://github.com/AlexTagg392/Taker-of-Notes" class="btn btn-primary"> Repo </a>
-                </div>
-              </div>
-            </div>
-            <hr />
+                <p>
+                  If you need to you can contact me at my email address:
+                </p>
+                <p>
+                  christag392@gmail.com
+                </p>  
+                <hr />    
           </Col>
         </Row>
       </Container>
@@ -116,3 +127,4 @@ function Projects() {
 }
 
 export default Projects;
+        
